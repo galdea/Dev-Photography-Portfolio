abfd1853fb4f836cafd2200b800f2eb5c02c9d59
Galdea <gabriel.aldea@gmail.com>
2023-05-11 18:24:26 +0000

    meetmeapp gif uploaded. missing 3. links and descriptions

--- src/data.js
+++ src/data.js
@@ -8,12 +8,12 @@ export const projects = [
     link: "https://reactbootcamp.com",
   },
   {
-    title: "React Tracks",
+    title: "MeetMeApp",
     subtitle: "React and Python",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "sasawsdfrgg",
   },
   {
     title: "DevChat",
