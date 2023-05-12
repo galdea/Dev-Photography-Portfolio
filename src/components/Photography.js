da0387d8ee4d5e3ebef163c65cdd5a47a25651c2
Galdea <gabriel.aldea@gmail.com>
2023-05-12 17:04:26 +0000

    work on contact form and photography.js

---
+++ src/components/Photography.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Photography() {
+  return (
+    <section>
+      <h1>I'm currently working on the site</h1>
+      <img src="./IMG_3266.jpg" alt="Photography" />
+    </section>
+  );
+}
