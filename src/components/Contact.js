bbae5451888d963c5b81843b6c85711142559f3e
Galdea <gabriel.aldea@gmail.com>
2023-05-15 16:41:54 +0000

    contact form details are ready. form working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,11 +13,19 @@ export default function Contact() {
       .join("&");
   }
 
- function handleSubmit(e) {
-  e.preventDefault();
-  alert("Message sent!");
-}
-
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => {
+        alert("Message sent, I will be contacting you soon!");
+        window.location.reload();
+      })
+      .catch((error) => alert(error));
+  }  
 
   return (
     <section id="contact" className="relative">
@@ -59,12 +67,10 @@ export default function Contact() {
           </div>
         </div>
         <form
-  name="contact"
-  method="POST"
-  data-netlify="true"
-  onSubmit={handleSubmit}
-  className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
->
+          netlify
+          name="contact"
+          onSubmit={handleSubmit}
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
 Let's talk          </h2>
           <p className="leading-relaxed mb-5">
